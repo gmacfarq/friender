5174124e9dae85710bcb1b0623e56dc90932e360
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-06-06 15:12:00 -0700

    setting up databse

--- friender-schema.sql
+++ friender-schema.sql
@@ -3,42 +3,41 @@ CREATE TABLE users (
   password TEXT NOT NULL,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
-  location INTEGER NOT NULL
+  location INTEGER NOT NULL,
+  hobbies TEXT NOT NULL,
+  interests TEXT NOT NULL
   email TEXT
     CHECK (position('@' IN email) > 1),
   is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE images (
-  id SERIAL PRIMARY KEY
-  handle VARCHAR(25) PRIMARY KEY CHECK (handle = lower(handle)),
-  name TEXT UNIQUE NOT NULL,
-  num_employees INTEGER CHECK (num_employees >= 0),
-  description TEXT NOT NULL,
-  logo_url TEXT
+  id SERIAL PRIMARY KEY,
+  file_name TEXT NOT NULL,
+  user_username VARCHAR(25) NOT NULL
+    REFERENCES users ON DELETE CASCADE
 );
 
 CREATE TABLE likes (
-  id SERIAL PRIMARY KEY,
-  title TEXT NOT NULL,
-  salary INTEGER CHECK (salary >= 0),
-  equity NUMERIC CHECK (equity <= 1.0),
-  company_handle VARCHAR(25) NOT NULL
-    REFERENCES companies ON DELETE CASCADE
+  username_liking VARCHAR(25)
+    REFERENCES users ON DELETE CASCADE,
+  username_liked VARCHAR(25)
+    REFERENCES users ON DELETE CASCADE,
+  match_id INTEGER
+    REFERENCES matches ON DELETE CASCADE,
+  PRIMARY KEY (username_liking, username_liked)
 );
 
 CREATE TABLE matches (
-  username VARCHAR(25)
-    REFERENCES users ON DELETE CASCADE,
-  job_id INTEGER
-    REFERENCES jobs ON DELETE CASCADE,
-  PRIMARY KEY (username, job_id)
+  id SERIAL PRIMARY KEY
+  timestamp DATE DEFAULT CURRENT_DATE NOT NULL,
 );
 
 CREATE TABLE messages (
-  username VARCHAR(25)
-    REFERENCES users ON DELETE CASCADE,
-  job_id INTEGER
-    REFERENCES jobs ON DELETE CASCADE,
-  PRIMARY KEY (username, job_id)
+  id SERIAL
+  match_id INTEGER
+    REFERENCES matches ON DELETE CASCADE,
+  timestamp DATE DEFAULT CURRENT_DATE NOT NULL,
+
+  PRIMARY KEY (id, match_id)
 );
