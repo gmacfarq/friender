e9b22bb93fbd2301ffb009f4b4c65103bbe30b8c
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-06-06 16:01:53 -0700

    final? database

--- friender-schema.sql
+++ friender-schema.sql
@@ -5,39 +5,29 @@ CREATE TABLE users (
   last_name TEXT NOT NULL,
   location INTEGER NOT NULL,
   hobbies TEXT NOT NULL,
-  interests TEXT NOT NULL
+  interests TEXT NOT NULL,
+  img_filename TEXT
   email TEXT
     CHECK (position('@' IN email) > 1),
   is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE images (
-  id SERIAL PRIMARY KEY,
-  file_name TEXT NOT NULL,
-  user_username VARCHAR(25) NOT NULL
-    REFERENCES users ON DELETE CASCADE
-);
-
-CREATE TABLE likes (
+CREATE TABLE matches (
+  id SERIAL PRIMARY KEY
   username_liking VARCHAR(25)
     REFERENCES users ON DELETE CASCADE,
   username_liked VARCHAR(25)
     REFERENCES users ON DELETE CASCADE,
-  match_id INTEGER
-    REFERENCES matches ON DELETE CASCADE,
-  PRIMARY KEY (username_liking, username_liked)
-);
-
-CREATE TABLE matches (
-  id SERIAL PRIMARY KEY
-  timestamp DATE DEFAULT CURRENT_DATE NOT NULL,
+  successful_match BOOLEAN DEFAULT FALSE
+  timestamp DATE DEFAULT NULL,
 );
 
 CREATE TABLE messages (
-  id SERIAL
+  id SERIAL,
   match_id INTEGER
     REFERENCES matches ON DELETE CASCADE,
+  user_sending_message VARCHAR(25)
+    REFERENCES users ON DELETE CASCADE,
   timestamp DATE DEFAULT CURRENT_DATE NOT NULL,
-
-  PRIMARY KEY (id, match_id)
+  PRIMARY KEY (id, match_id, user_sending_message)
 );
