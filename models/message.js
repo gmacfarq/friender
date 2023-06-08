ec308c79d9e0c6ed74b6955511b385fd5c274b71
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-06-07 17:01:07 -0700

    messages docstrings

--- models/message.js
+++ models/message.js
@@ -11,8 +11,28 @@ const {
 
 const { BCRYPT_WORK_FACTOR } = require("../config.js");
 
+/** Related functions for messages. */
+
 class Message {
 
+  /** Create a message between two users.
+   *
+   * @param {obj} data
+   *       { receiverUsername,
+   *         senderUsername,
+   *         messageText,
+   *         messageDate
+   *       }
+   *
+   * @returns {obj}
+   *        { messageId,
+   *           recieverUsername,
+   *           senderUsername,
+   *           messageText,
+   *           messageDate
+   *         }
+   *
+   **/
 
   static async create(data) {
     const result = await db.query(`
@@ -23,26 +43,36 @@ class Message {
       VALUES ($1, $2, $3, $4)
       RETURNING
         message_id AS "messageId",
-        receiver_username AS  "receiverUsername",npm 
+        receiver_username AS  "receiverUsername",npm
         sender_username AS "senderUsername",
-      message_text AS "messageText",
-      message_date AS "messageDate"
-        `, [
-      data.receiverUsername,
-      data.senderUsername,
-      data.messageText,
-      data.messageDate
-    ]);
+        message_text AS "messageText",
+        message_date AS "messageDate"`,
+      [
+        data.receiverUsername,
+        data.senderUsername,
+        data.messageText,
+        data.messageDate
+      ]);
 
     const message = result.rows[0];
 
     return message;
   }
 
-
+   /** Get a message from the database.
+   *
+   * @param {number} id
+   *
+   * @returns {obj}
+   *        { messageId,
+   *           recieverUsername,
+   *           senderUsername,
+   *           messageText,
+   *           messageDate
+   *         }
+   **/
 
   static async get(id) {
-
     const messageRes = await db.query(`
     SELECT message_id AS "messageId",
           receiver_username AS  "receiverUsername",
@@ -59,8 +89,6 @@ class Message {
     return message;
 
   }
-
-
 }
 
 module.exports = Message;
